fec33748cfd76a294784f5b52b9ffacd99644263
fish <shayancyler1612@gmail.com>
2023-12-16 22:02:16 +0330

    edit task model

--- task/models.py
+++ task/models.py
@@ -7,7 +7,17 @@ User = get_user_model()
 
 
 class Task(BaseModel):
+    HIGH = 1
+    MIDDLE = 2
+    LOW = 3
+    priority_level = (
+        (HIGH, 'high'),
+        (MIDDLE, 'middle'),
+        (LOW, 'low'),
+    )
+
     title = models.TextField()
     description = models.TextField()
     user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='tasks')
-    is_done = models.BooleanField()
+    is_done = models.BooleanField(default=False)
+    priority = models.PositiveSmallIntegerField(choices=priority_level, default=None)
